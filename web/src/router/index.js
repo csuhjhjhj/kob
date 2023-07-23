08dd3b6b6793d4ab0aa73999e3fc3d07cd8cc163
Enternity <1619842189@qq.com>
2023-07-23 13:52:40 +0800

    创建web端和acapp端

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const routes = [
